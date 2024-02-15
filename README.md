b55cf2bebce376aad86c88c48a39769b2f5af60f
webcatzz <crowsthyme@gmail.com>
2024-02-15 19:20:59 +0400

    tweaks + beepbox ring

--- README.md
+++
@@ -1,3 +0,0 @@
-the code for my site, **[webcatz.neocities.org](https://webcatz.neocities.org/)**!
-
-currently in: v5.1
---
+++ neocities/beepbox-webring/ring.js
@@ -0,0 +1,44 @@
+bpSites = [
+  "https://webcatz.neocities.org/",
+  "https://kelprot.neocities.org/",
+  "https://murumart.neocities.org/m/jummbox_experiments",
+  "https://troy-sucks.neocities.org/beepbox",
+  "https://ninacti0n.neocities.org/",
+  "https://mikaorangeart.neocities.org/",
+  "https://robophobia.org/",
+  "https://fizzsea.neocities.org/",
+  "https://tim-of-the-valley.neocities.org/",
+  "https://foggybear42.neocities.org/",
+  "https://mechagic.neocities.org/",
+  "https://sawtooth.neocities.org/",
+];
+
+
+bpIdx = bpSites.findIndex(url => location.href.startsWith(url));
+if (bpIdx == -1) document.currentScript.outerHTML = "<div style=\"color: #999; font: 0.75em sans-serif; padding: 0.5em; border: 4px solid #222; border-radius: 5px; background-color: black\">this site isn't part of the the beepbox webring yet :(</div>";
+// song widget
+else if (document.currentScript.dataset.song) {
+  document.currentScript.outerHTML = `
+    <div id="beepbox-webring">
+      <a class="beepbox-arrow" href="${bpSites.at(bpIdx - 1)}"><img src="https://webcatz.neocities.org/beepbox-webring/left.png" alt="left arrow"></a>
+      <div id="beepbox-main">
+        <div>this site is part of the <a href="https://webcatz.neocities.org/beepbox-webring/">beepbox webring</a>!</div>
+        <iframe src="${document.currentScript.dataset.song}"></iframe>
+      </div>
+      <a class="beepbox-arrow" href="${bpSites[bpIdx + 1 % bpSites.length]}"><img src="https://webcatz.neocities.org/beepbox-webring/right.png" alt="right arrow"></a>
+    </div>
+  `;
+  let sheet = document.createElement("link");
+  sheet.rel = "stylesheet", sheet.href = "https://webcatz.neocities.org/beepbox-webring/song.css";
+  document.head.appendChild(sheet);
+}
+// button widget
+else document.currentScript.outerHTML = `
+  <div id="beepbox-webring" style="display: flex; align-items: center; gap: 8px">
+    <a href="${bpSites.at(bpIdx - 1)}"><img src="https://webcatz.neocities.org/beepbox-webring/button-left.png" alt="prev"></a>
+    <a href="https://webcatz.neocities.org/beepbox-webring/"><img src="https://webcatz.neocities.org/beepbox-webring/button.png" alt="beepbox webring"></a>
+    <a href="${bpSites[bpIdx + 1 % bpSites.length]}"><img src="https://webcatz.neocities.org/beepbox-webring/button-right.png" alt="next"></a>
+  </div>
+`;
+
+delete bpIdx;
