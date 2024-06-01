13aac8caa51d64a7a5f3df8888feda9a399d6a6f
webcatz <june.webcatz@gmail.com>
2024-06-01 15:16:50 +0400

    new /about

--- neocities/about/about.js
+++ neocities/about/about.js
@@ -1,76 +1,48 @@
-const select = document.getElementById("player-select");
+// audio player
+
+const player = document.getElementById("player");
+let audioEls = player.children;
 AudioPlayer.create([
-	"assisted harakiri - home is where",
-	"bedroom community - glass beach",
-	"call me pretty - echinacea",
-	"calico - ok glass",
-	"change your name - see through person",
-	"come in - weatherday",
-	"commatose - glass beach",
 	"dallas - glass beach",
-	"fake violins - casio dad",
-	"foe paw - the j arthur keenes band",
-	"our story - stomach book",
-	"pipeline punch - newgrounds death rugby",
-	"slip under the door - glass beach",
+	"motions - glass beach",
+	"rare animal - glass beach",
+	"200 - glass beach",
+	"commatose - glass beach",
+	"speedway - black midi",
+	"dethroned - black midi",
+	"still - black midi",
+	"change your name - see through person",
+	"periwinkle - see through person",
+	"calico - ok glass",
+	// "foe paw - the j arthur keenes band",
 	"subterranean homesick alien - radiohead",
-	"zipper - mover shaker",
 	"心に雲を持つ少年 - sunny day service",
+	"five finger exploding heart technique - ok cool",
+	"saucy!!! - red sun",
+	"portrait of a woman on a couch with cats - michael cera palin",
+	"paper planes - twikipedia",
+	"eat me alive - saturdays at your place",
+	"mother mary - late bloomer",
 ],{
-	playButton: document.getElementById("player-button"),
+	playButton: audioEls[0],
+	skipButton: audioEls[1],
+	title: audioEls[2],
 	insertIcons: true,
-	slider: document.getElementById("player-slider"),
-	sliderThumb: document.getElementById("player-slider").querySelector("div:last-of-type"),
 	shuffle: true,
-	fileGarden: "ZdmFgugxzVCR-8Bl",
-	onended: () => select.value = AudioPlayer.idx
+	fileGarden: "ZdmFgugxzVCR-8Bl"
 });
-for (let i = 0; i < AudioPlayer.list.length; i++) {
-	let option = select.appendChild(document.createElement("option"));
-	option.label = AudioPlayer.list[i].title, option.value = i;
+
+// title ꕀ
+for (const track of AudioPlayer.list) track.title = track.title.replace(" - ", " ꕀ ");
+audioEls[2].textContent = audioEls[2].textContent.replace(" - ", " ꕀ ");
+
+
+
+// intro marquee
+let marquee = document.getElementById("intro-scroll");
+let spanned = "";
+for (const letter of marquee.textContent) {
+	if (letter === " ") spanned += " ";
+	else spanned += "<span>" + letter + "</span>";
 }
-select.onchange = () => {
-	AudioPlayer.idx = Number(select.value) - 1;
-	AudioPlayer.next();
-}
-
-// perspective origin
-const main = document.getElementById("main");
-let rect = main.getBoundingClientRect();
-let centerX = rect.left + rect.width / 2;
-let centerY = rect.top + document.body.offsetHeight / 2;
-let centerXOff = rect.width / 2;
-onmousemove = e => {
-	let x = (centerX - e.x) / 5 + centerXOff;
-	let y = (centerY - e.y) / 5;
-	main.style.perspectiveOrigin = x + "px " + y + "px";
-}
-
-
-// toggling perspective
-function togglePerspective() {
-	document.getElementById("perspective-toggle").checked ? main.removeAttribute("style") : main.style.perspective = "none";
-}
-document.getElementById("perspective-toggle").onchange = togglePerspective;
-togglePerspective();
-
-
-// strawberry
-document.getElementById("strawberry").onclick = function () {
-	let sfx = new Audio("https://files.catbox.moe/fj0udt.wav");
-	sfx.onloadeddata = () => {
-		this.style.animation = "2.75s fly 0.5s forwards";
-	}
-	sfx.play();
-
-	function intercept(e) {e.stopImmediatePropagation()}
-}
-
-// explode
-document.getElementById("explode").onclick = e => {
-	let gif = document.createElement("img");
-	gif.src = "assets/spr_realisticexplosion.gif";
-	gif.style.position = "fixed", gif.style.left = e.x - 35.5 + "px", gif.style.top = e.y - 55 + "px", gif.style.pointerEvents = "none";
-	document.body.appendChild(gif);
-	setTimeout(() => gif.remove(), 1000);
-}
+marquee.innerHTML = spanned;
