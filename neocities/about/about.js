1a5a61b56a052111edae3fd99635be9714d71eac
webcatz <june.webcatz@gmail.com>
2024-06-20 11:55:35 +0400

    /about playlist

--- neocities/about/about.js
+++ neocities/about/about.js
@@ -1,18 +1,15 @@
 // audio player
 
 const player = document.getElementById("player");
-let audioEls = player.children;
 AudioPlayer.create([
 	"dallas - glass beach",
 	"motions - glass beach",
 	"rare animal - glass beach",
 	"200 - glass beach",
-	"commatose - glass beach",
 	"speedway - black midi",
 	"dethroned - black midi",
 	"still - black midi",
 	"change your name - see through person",
-	"periwinkle - see through person",
 	"calico - ok glass",
 	"subterranean homesick alien - radiohead",
 	"心に雲を持つ少年 - sunny day service",
@@ -24,17 +21,17 @@ AudioPlayer.create([
 	"mother mary - late bloomer",
 	"chainsaw girl - chainsaw girl",
 ],{
-	playButton: audioEls[0],
-	skipButton: audioEls[1],
-	title: audioEls[2],
+	playButton: player.children[0],
+	skipButton: player.children[1],
+	title: player.children[2],
 	insertIcons: true,
-	shuffle: true,
+	// shuffle: true,
 	fileGarden: "ZdmFgugxzVCR-8Bl"
 });
 
 // title ꕀ
 for (const track of AudioPlayer.list) track.title = track.title.replace(" - ", " ꕀ ");
-audioEls[2].textContent = audioEls[2].textContent.replace(" - ", " ꕀ ");
+player.children[2].textContent = player.children[2].textContent.replace(" - ", " ꕀ ");
 
 
 
