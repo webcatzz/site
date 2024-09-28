a84a676098e039bbcd5345ccfcbf9bf0945aa7ba
webcatzz <june.webcatz@gmail.com>
2024-09-28 03:48:06 -0400

    bpring update

--- src/beepbox-webring/ring.js
+++ src/beepbox-webring/ring.js
@@ -1,7 +1,7 @@
 bpSites = [
 	"https://webcatz.neocities.org",
 	"https://kelprot.neocities.org",
-	"https://murumart.neocities.org/m/jummbox_experiments",
+	"https://murumart.neocities.org/m/beeps/",
 	"https://troy-sucks.neocities.org/beepbox",
 	"https://ninacti0n.neocities.org",
 	"https://mikaorangeart.neocities.org",
