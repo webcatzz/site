d29131ab25413727ebd6017bdf9089bcb1c94e53
webcatzz <june.webcatz@gmail.com>
2024-12-29 15:48:56 +0400

    bd uses main

--- neocities/made/bad-dream/script.js
+++ neocities/made/bad-dream/script.js
@@ -1,15 +1,15 @@
-const wrapper = document.getElementById("wrapper");
-const articles = wrapper.getElementsByTagName("article");
+const main = document.querySelector("main");
+const articles = main.getElementsByTagName("article");
 const nav = document.querySelector("nav");
 
-wrapper.onscroll = () => {
+main.onscroll = () => {
 	let i = -1;
 	for (const article of articles) {
 		i++;
-		if (article.offsetTop + article.offsetHeight > wrapper.scrollTop) break;
+		if (article.offsetTop + article.offsetHeight > main.scrollTop) break;
 	}
 	nav.getElementsByClassName("active")[0]?.classList.remove("active");
 	nav.children[i].classList.add("active");
 }
 
-wrapper.onscroll();
+main.onscroll();
