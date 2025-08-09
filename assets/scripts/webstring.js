0556b9862545236bd10e51ca0c58119b634164de
webcatzz <71499717+webcatzz@users.noreply.github.com>
2025-08-09 15:17:03 +0400

    /resources: minor webstring refactor

--- assets/scripts/webstring.js
+++ assets/scripts/webstring.js
@@ -1,27 +1,25 @@
 // webstring by june @ juneish.neocities.org
 
 {
-	let webring = {
-		// sites in the ring
-		sites: [
-			"https://example.com",
-			"https://example.com",
-		],
-		// widget html
-		// PREV and NEXT are replaced with neighbors' urls
-		widgets: {
-			default: `<div id="my-webring" style="display: flex; gap: 8px">
-				<a href="PREV">prev</a>
-				<div>webring</div>
-				<a href="NEXT">next</a>
-			</div>`,
-			error: `<div>this site isn't part of the webring yet</div>`,
-		},
+	// sites in the ring
+	let sites = [
+		"https://example.com",
+		"https://example.com",
+	];
+	// widget html
+	// PREV and NEXT are replaced with neighbors' urls
+	let widgets = {
+		default: `<div id="my-webring" style="display: flex; gap: 8px">
+			<a href="PREV">prev</a>
+			<div>webring</div>
+			<a href="NEXT">next</a>
+		</div>`,
+		error: `<div>this site isn't part of the webring yet</div>`,
 	};
 	// code
-	webring.idx = webring.sites.findIndex(site => location.href.startsWith(site));
-	document.currentScript.outerHTML = webring.idx === -1 ? webring.widgets.error :
-		(webring.widgets[document.currentScript.dataset.widget] ?? webring.widgets.default)
-		.replace("PREV", webring.sites.at(webring.idx - 1))
-		.replace("NEXT", webring.sites[(webring.idx + 1) % webring.sites.length]);
+	let idx = sites.findIndex(site => location.href.startsWith(site));
+	document.currentScript.outerHTML = idx === -1 ? widgets.error :
+		(widgets[document.currentScript.dataset.widget] ?? widgets.default)
+		.replace("PREV", sites.at(idx - 1))
+		.replace("NEXT", sites[(idx + 1) % sites.length]);
 }
