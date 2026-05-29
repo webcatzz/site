4b12cde12d6bd835fb1c5db8272efa73f031eef0
webcatzz <71499717+webcatzz@users.noreply.github.com>
2026-05-29 12:00:35 +0400

    /comics/: style as zine

--- site/comics/script.js
+++ site/comics/script.js
@@ -1,13 +1,25 @@
-for (const article of document.getElementsByTagName("article")) {
-	let btn = article.querySelector(".comments-btn");
-	let comments = article.querySelector(".comments");
-	if (!btn || !comments) continue;
-	btn.addEventListener("click", function() {
-		if (comments.classList.toggle("hidden")) {
-			btn.textContent = "(show comments)";
-		}
-		else {
-			btn.textContent = "(hide comments)";
-		}
-	});
+const main = document.getElementById("main");
+
+// rotates zine subtly to angle toward cursor
+main.addEventListener("mousemove", function(e) {
+	let rotX = (e.y - (this.offsetTop + this.offsetHeight / 2)) * 0.01;
+	let rotY = (e.x - (this.offsetLeft + this.offsetWidth / 2)) * -0.005;
+	this.style.transform = `rotateX(${rotX}deg) rotateY(${rotY}deg)`;
+});
+
+// resets zine rotation when cursor leaves
+main.addEventListener("mouseleave", function() {
+	this.style.transform = "none";
+});
+
+// navigates with keyboard
+addEventListener("keydown", e => {
+	if (e.key == "ArrowLeft")  { flip("left");  }
+	if (e.key == "ArrowRight") { flip("right"); }
+});
+
+// navigates using the left or right page
+function flip(dir) {
+	let current_section = location.hash ? document.getElementById(location.hash.slice(1)) : main.firstElementChild;
+	current_section.querySelector(`& > .${dir} > a`)?.click();
 }
